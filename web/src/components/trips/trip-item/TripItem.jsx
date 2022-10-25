64a3a45d0b4f6b3d5c27b35eb40a87fbc6a458ea
Almudena <almudena.vigara@gmail.com>
2022-10-25 11:52:37 +0200

    fix: TripItem style

--- web/src/components/trips/trip-item/TripItem.jsx
+++ web/src/components/trips/trip-item/TripItem.jsx
@@ -1,27 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import moment from 'moment';
+import moment from "moment";
 import "./TripItem.css";
 
-function TripItem({ city, coverPhoto, startDate, endDate,id }) {
-
+function TripItem({ city, coverPhoto, startDate, endDate, id }) {
   return (
     <Link to={`/${id}`}>
       <div className="card text-white border-0 mx-auto">
         <img className="card-img rounded" src={coverPhoto} alt={city} />
         <div className="card-img-overlay">
-          <h1 className="card-title d-flex justify-content-center h1-card">{city} </h1>
-          <h5 className="card-title d-flex justify-content-center h1-card">{moment(startDate).format("D MMM YYYY")} <i className="date-arrow fa-solid fa-arrow-right-long" />{moment(endDate).format("D MMM YYYY")}</h5>
-          <h5 className="card-title d-flex justify-content-center h1-card">{moment(startDate).fromNow()} </h5>
-
-          {/* <p className="card-text p-card">
-            This is a wider card with supporting text below 
-          </p>
-          <p className="card-text p-card">Poner aqui countdown</p> */}
+          <div className="d-flex justify-content-between">
+          <h1 className="card-title d-flex h1-card text-capitalize">
+            {city}
+          </h1>
+          <h6 className="card-title d-flex justify-content-center h1-card">
+            {moment(startDate).fromNow()}
+          </h6>
+          </div>
+          
+          <h6 className="card-title d-flex h1-card">
+            {moment(startDate).format("D MMM YYYY")}
+            <i className="date-arrow fa-solid fa-arrow-right-long" />
+            {moment(endDate).format("D MMM YYYY")}
+          </h6>
+          
         </div>
-        
       </div>
-
     </Link>
   );
 }
