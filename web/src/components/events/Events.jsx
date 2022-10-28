1266339ad260c92f0171416d257cf5fbd1dc956b
Almudena <almudena.vigara@gmail.com>
2022-10-28 14:20:23 +0200

    fix: style events

--- web/src/components/events/Events.jsx
+++ web/src/components/events/Events.jsx
@@ -1,23 +1,29 @@
 import React from "react";
-import moment from "moment"
+import moment from "moment";
+import './Events.css'
 
 export default function Events(props) {
   return (
     <>
-      {/* {console.log(props)} */}
-      <div></div>
-      <div className="card" style={{ width: 12 + "rem" }}>
-        <img src={props.images[1].url} className="card-img-top" alt={props.element} />
-        <div className="card-body">
-          <h5 className="card-title">{props.name}</h5>
-          <p className="card-text">
-            {moment(props.dates.start.dateTime).format("D MMM YYYY")}
-            <br />
-            {props._embedded.venues[0].name}
-          </p>
-          <a href={props.url} target="_blank" rel="noreferrer" className="btn btn-primary">
-            Go to event
-          </a>
+      <div className="border-box d-flex justify-content-between bg-white mb-2 rounded p-2 text-capitalize">
+        <div className="d-flex ">
+          <img
+            src={props.images[1].url}
+            className="w-25 rounded fill"
+            alt={props.element}
+          />
+          <div className="d-flex flex-column event-info pt-2 ">
+            <h6 className="card-title event-info text-dark">{props.name}</h6>
+            <p className="card-text text-muted event-info">
+              {moment(props.dates.start.dateTime).format("D MMM YYYY")}
+              <br />
+              {props._embedded.venues[0].name}
+            </p>
+            {/* see file btn: */}
+            <a href={props.url} target="blank" className="link-event">
+              <i className="fa-regular fa-eye list-see-icon"></i> <span className="text-dark">Ver evento</span>
+            </a>
+          </div>
         </div>
       </div>
     </>
